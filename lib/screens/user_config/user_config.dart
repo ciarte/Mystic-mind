e863bb2107af0f629afc41cb5bef99f4d532056f
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-11-19 18:32:24 -0300

    menu signos actualizado

--- lib/screens/user_config/user_config.dart
+++ lib/screens/user_config/user_config.dart
@@ -10,11 +10,17 @@ import 'package:horoscope_app/screens/horoscope/sign_selection_screen.dart';
 import 'package:horoscope_app/widgets/widgets.dart';
 import '../../services/local_storage.dart';
 
-class UserConfigScreen extends ConsumerWidget {
+class UserConfigScreen extends ConsumerStatefulWidget {
   const UserConfigScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  UserConfigScreenState createState() => UserConfigScreenState();
+}
+
+class UserConfigScreenState extends ConsumerState<UserConfigScreen> {
+  int selectedZodiacIndex = -1;
+  @override
+  Widget build(BuildContext context) {
     final isDarkmode = ref.watch(darkModeProvider);
     final selectedLanguage = ref.watch(currentLanguageProvider);
 
@@ -25,61 +31,28 @@ class UserConfigScreen extends ConsumerWidget {
       child: SingleChildScrollView(
         child: Column(children: [
           HeadingWidget(title: texts.tConfig),
-          //  const Spacer(flex:1),
-          IconButton(
-            icon: Icon(
-                isDarkmode
-                    ? Icons.dark_mode_outlined
-                    : Icons.light_mode_outlined,
-                size: 35),
-            onPressed: () {
-              // LocalStorage.prefs.setBool('selectedMode', isDarkmode);
-              ref.read(darkModeProvider.notifier).toggleDarkMode();
-            },
-          ),
-
-          Text(
-            texts.tAlias,
-            style: GoogleFonts.macondo(
-              fontSize: 32,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          TextFormField(
-            controller: nameController,
-            onFieldSubmitted: (String value) {
-              LocalStorage.prefs.setString('alias', value);
-              final keys = LocalStorage.prefs.getKeys();
-              for (String key in keys) {
-                print(LocalStorage.prefs.getString('selectedLanguage'));
-                print('$key: ${LocalStorage.prefs.get(key)}');
-              }
-            },
-          ),
-
-          Text(
-            texts.tSign,
-            style: GoogleFonts.macondo(
-              fontSize: 32,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(
-            height: 380,
-            child: GridView.builder(
-              controller: ScrollController(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, // 2 tarjetas por columna
-                crossAxisSpacing: 15, // Espacio entre columnas
-                mainAxisSpacing: 8, // Espacio entre filas
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                texts.tMode,
+                style: GoogleFonts.macondo(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              itemCount: getZodiacItems(context).length,
-              itemBuilder: (BuildContext context, int index) {
-                final zodiacItem = getZodiacItems(context)[index];
-                return _CustomListTile(menuItem: zodiacItem);
-              },
-            ),
+              IconButton(
+                icon: Icon(
+                    isDarkmode
+                        ? Icons.dark_mode_outlined
+                        : Icons.light_mode_outlined,
+                    size: 35),
+                onPressed: () {
+                  // LocalStorage.prefs.setBool('selectedMode', isDarkmode);
+                  ref.read(darkModeProvider.notifier).toggleDarkMode();
+                },
+              ),
+            ],
           ),
           Column(
             children: [
@@ -121,48 +94,90 @@ class UserConfigScreen extends ConsumerWidget {
               ),
             ],
           ),
-          const SizedBox(
-            height: 50,
-          )
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+          Text(
+            texts.tAlias,
+            style: GoogleFonts.macondo(
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          TextFormField(
+            controller: nameController,
+            onFieldSubmitted: (String value) {
+              LocalStorage.prefs.setString('alias', value);
+              final keys = LocalStorage.prefs.getKeys();
+              for (String key in keys) {
+                print(LocalStorage.prefs.getString('selectedLanguage'));
+                print('$key: ${LocalStorage.prefs.get(key)}');
+              }
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+          Text(
+            texts.tSign,
+            style: GoogleFonts.macondo(
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 300,
+            child: GridView.builder(
+              controller: ScrollController(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // 2 tarjetas por columna
+                // crossAxisSpacing: 5, // Espacio entre columnas
+                // mainAxisSpacing: 1, // Espacio entre filas
+              ),
+              itemCount: getZodiacItems(context).length,
+              itemBuilder: (BuildContext context, int index) {
+                final zodiacItem = getZodiacItems(context)[index];
+                final isSelected = index == selectedZodiacIndex;
+                return ListTile(
+                    selected: isSelected,
+                    title: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Opacity(
+                        opacity: isSelected ? 1.0 : 0.50,
+                        child: Image.asset(
+                          zodiacItem.image,
+                          alignment: Alignment.topCenter,
+                          fit: BoxFit.cover,
+                          height: 60,
+                          width: 70,
+                        ),
+                      ),
+                    ),
+                    subtitle: Text(
+                      zodiacItem.title,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: !isSelected ? Colors.black54 : Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        selectedZodiacIndex = isSelected
+                            ? -1
+                            : index; // Si ya está seleccionado, deselecciónalo; de lo contrario, selecciónalo
+                      });
+                    });
+              },
+            ),
+          ),
         ]),
       ),
     );
-  }
-}
-
-class _CustomListTile extends StatelessWidget {
-  final MenuItem menuItem;
-
-  const _CustomListTile({required this.menuItem});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Card(
-          child: InkWell(
-        onTap: () {
-          LocalStorage.prefs.setString('selectedSign', menuItem.location);
-        },
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.asset(
-            menuItem.image,
-            alignment: Alignment.topCenter,
-            fit: BoxFit.cover,
-            height: 90,
-            width: 90,
-          ),
-        ),
-      )),
-      Text(
-        menuItem.title,
-        style: const TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    ]);
   }
 }
 
